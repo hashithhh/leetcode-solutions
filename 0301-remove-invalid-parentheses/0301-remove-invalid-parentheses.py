89d025b76d41324516c92cdf17eaf0ce17ccda00
hashithhh <hashithalakunta907@gmail.com>
2026-07-24 21:48:15 +0530

    Time: 79 ms (58.88%), Space: 20.1 MB (34.1%) - LeetHub

---
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.py
@@ -0,0 +1,40 @@
+class Solution:
+    def removeInvalidParentheses(self, s: str) -> List[str]:
+
+        def is_valid(s):
+            open= 0
+            for i in range(len(s)):
+                if s[i] == '(':
+                    open += 1
+                if s[i] == ')':
+                    open -= 1
+                if open < 0:
+                    return False
+            if open != 0:
+                return False
+            return True
+        
+        queue = deque([s])
+        visited = set()
+        visited.add(s)
+        result = []
+        found = False
+        
+        while queue:
+            for _ in range(len(queue)):
+                cur_s = queue.popleft()
+                if is_valid(cur_s):
+                    result.append(cur_s)
+                    found = True
+                if found:
+                    continue
+                
+                for i in range(len(cur_s)):
+                    if cur_s[i] =='(' or cur_s[i] == ')':
+                        new_s = cur_s[:i] + cur_s[i+1 :]
+                        if new_s not in visited:
+                            visited.add(new_s)
+                            queue.append(new_s)
+            if found:
+                return result
+        return [""]
